804b153107dda653e9b38456c43ebce3ea4a0289
Nandor POKA <nandor.poka@urbanandmainlines.com>
2025-03-12 12:27:19 +0100

    minor cleanup and code fixes

--- Frontend/src/components/Playlists.vue
+++ Frontend/src/components/Playlists.vue
@@ -9,37 +9,13 @@ const axios: Axios = inject('axios') as Axios
 const playlistStore = PlaylistStore()
 const trackStore = TrackStore()
 const playlistSelector = useTemplateRef('playlistSelector')
-const dbSelector = useTemplateRef('dbSelector')
-const updteDb = useTemplateRef('updateDB')
+//const dbSelector = useTemplateRef('dbSelector')
+//const updteDb = useTemplateRef('updateDB')
 
 onMounted(() => {
-  if (playlistStore.playlistDatabasePath != '') {
-    if (dbSelector.value) {
-      if (dbSelector.value.value) {
-        axios
-          .post(
-            'addDataSource',
-            {
-              path: dbSelector.value.value,
-            },
-            {
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-              },
-            },
-          )
-          .then(function (response) {
-            const result = response.data
-            if (result.dbInitialized) {
-              if (dbSelector.value) {
-                if (dbSelector.value.files) {
-                  playlistStore.playlistDatabasePath = dbSelector.value.value
-                }
-              }
-
-              axios
-                .get('getPlaylists', {
-                  method: 'get',
+  axios
+    .get('getPlaylists', {
+      method: 'get',
                   headers: {
                     'Content-Type': 'application/json',
                   },
@@ -52,66 +28,6 @@ onMounted(() => {
                   // handle error
                   console.log(error)
                 })
-            }
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error)
-          })
-      }
-    }
-  }
-
-  if (updteDb.value) {
-    updteDb.value.onclick = function () {
-      if (dbSelector.value) {
-        if (dbSelector.value.value) {
-          axios
-            .post(
-              'addDataSource',
-              {
-                path: dbSelector.value.value,
-              },
-              {
-                headers: {
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                },
-              },
-            )
-            .then(function (response) {
-              const result = response.data
-              if (result.dbInitialized) {
-                if (dbSelector.value) {
-                  if (dbSelector.value.files) {
-                    playlistStore.playlistDatabasePath = dbSelector.value.value
-                  }
-                }
-
-                axios
-                  .get('getPlaylists', {
-                    method: 'get',
-                    headers: {
-                      'Content-Type': 'application/json',
-                    },
-                  })
-                  .then(function (response) {
-                    const playlits = response.data
-                    playlistStore.playlists = playlits
-                  })
-                  .catch(function (error) {
-                    // handle error
-                    console.log(error)
-                  })
-              }
-            })
-            .catch(function (error) {
-              // handle error
-              console.log(error)
-            })
-        }
-      }
-    }
-  }
 
   if (playlistSelector.value) {
     playlistSelector.value.oninput = function () {
@@ -139,20 +55,10 @@ onMounted(() => {
   <Navbar />
   <h1>Playlists</h1>
   <div>
-    <label for="dbSelector">Select database file:</label>
-    <input
-      type="text"
-      ref="dbSelector"
-      name="dbSelector"
-      id="dbSelector"
-      v-bind:value="playlistStore.playlistDatabasePath"
-    />
-    <input type="button" value="Update Database" ref="updateDB" />
-    <label v-if="playlistStore.playlistDatabasePath != ''" for="playlistSelector"
-      >Available playlists:</label
-    >
+    
+    <label for="playlistSelector"
+      >Available playlists:</label>
     <select
-      v-if="playlistStore.playlistDatabasePath != ''"
       ref="playlistSelector"
       name="playlistSelector"
       id="playlistSelector"
