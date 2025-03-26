27a7d7c612c6a492315f5a32aba13df29df11be1
Nandor POKA <nandor.poka@urbanandmainlines.com>
2025-03-26 09:41:00 +0100

    small clean up and refactored RecommendationController code

--- MainServer/src/main/java/com/openstreamingtools/MainServer/controllers/RecommendationController.java
+++ MainServer/src/main/java/com/openstreamingtools/MainServer/controllers/RecommendationController.java
@@ -1,6 +1,7 @@
 package com.openstreamingtools.MainServer.controllers;
 
 import com.openstreamingtools.MainServer.db.entities.Track;
+import com.openstreamingtools.MainServer.db.repositories.PlaylistRepository;
 import com.openstreamingtools.MainServer.db.repositories.TrackRepository;
 import com.openstreamingtools.MainServer.twitch.TwitchUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,41 +16,47 @@ import java.util.List;
 @RestController
 public class RecommendationController {
 
+    private final String twitchMessageHeader = "I recommend the following songs to you to request:\n";
 
     @Autowired
     private TrackRepository trackRepository;
 
     @GetMapping(value = "/api/getInKeyRecommendation/{key}", produces = "application/json")
     public void getInKeyRecommendation(@PathVariable int key){
-        List<Track> tracks = trackRepository.findAllByPlaylistID(17);
+        StringBuilder twitchMessage = new StringBuilder(twitchMessageHeader);
+        int i=1;
+        for (Track track : getInKeyTracks(key).subList(0, 5)) {
+            twitchMessage.append(i++).append(". ").append(track.getTitle()).append(" - ").append(track.getArtist()).append(",\n");
+        }
+        TwitchUtils.sendToChat(twitchMessage.toString());
+    }
+
+    @GetMapping(value = "/api/getBroadInKeyRecommendation/{key}", produces = "application/json")
+    public void getInBroadKeyRecommendation(@PathVariable int key){
+        StringBuilder twitchMessage = new StringBuilder(twitchMessageHeader);
+        int i=1;
+        for (Track track : getInKeyTracks(key).subList(0, 3)) {
+            twitchMessage.append(i++).append(". ").append(track.getTitle()).append(" - ").append(track.getArtist()).append(",\n");
+        }
+        for (Track track : getInKeyTracks(key+1).subList(0, 2)) {
+            twitchMessage.append(i++).append(". ").append(track.getTitle()).append(" - ").append(track.getArtist()).append(",\n");
+        }
+        for (Track track : getInKeyTracks(key-1).subList(0, 2)) {
+            twitchMessage.append(i++).append(". ").append(track.getTitle()).append(" - ").append(track.getArtist()).append(",\n");
+        }
+
+    }
+
+    private List<Track> getInKeyTracks(int key){
         List<Track> tracksInKey = new ArrayList<>();
-        for (Track track : tracks){
+        for (Track track : trackRepository.findAllByPlaylistID(17)){
             if(track.getKey() == key){
                 tracksInKey.add(track);
             }
         }
         Collections.shuffle(tracksInKey);
-        StringBuilder twitchMessage = new StringBuilder("I recommend the following songs to you to request:\n");
-        int i=1;
-        for (Track track :  tracksInKey.subList(0, 5)) {
-            twitchMessage.append(i++).append(". ").append(track.getTitle()).append(" - ").append(track.getArtist()).append(",\n");
-        }
-        TwitchUtils.sendToChat(twitchMessage.toString());
+        return tracksInKey;
 
-      // return tracks.subList(0, 5);
     }
 
-    @GetMapping(value = "/api/getBroadInKeyRecommendation/{key}", produces = "application/json")
-    public List<Track> getInBroadKeyRecommendation(@PathVariable int key){
-        List<Track> inKeyTracks = trackRepository.findByKey(key);
-        List<Track> inKeyBeforeTracks = trackRepository.findByKey(key-1);
-        List<Track> inKeyAfterTracks = trackRepository.findByKey(key+1);
-        Collections.shuffle(inKeyTracks);
-        Collections.shuffle(inKeyBeforeTracks);
-        Collections.shuffle(inKeyAfterTracks);
-        List<Track> tracks = inKeyTracks.subList(0, 3);
-        tracks.addAll(inKeyBeforeTracks.subList(0, 2));
-        tracks.addAll(inKeyAfterTracks.subList(0, 2));
-        return tracks;
-    }
 }
