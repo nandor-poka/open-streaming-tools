d687a05eab06eaee3b7720814564c6cc3653f630
Nandor Poka <nandor@nand-or.com>
2025-08-15 23:00:20 +0200

    added shutodwn hook for ui schedule thread for gracefully freeing up resources

--- MainServer/src/main/java/com/openstreamingtools/MainServer/utils/UIUpdateScheduler.java
+++ MainServer/src/main/java/com/openstreamingtools/MainServer/utils/UIUpdateScheduler.java
@@ -1,15 +1,21 @@
 package com.openstreamingtools.MainServer.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static com.openstreamingtools.MainServer.config.OSTConfiguration.settings;
 
+@Slf4j
 public class UIUpdateScheduler implements Runnable{
 
     @Override
     public void run() {
-        try {
-            Utils.timer.schedule(Utils.taskQueue.take(),settings.getShowTrackDelay() * 1000L );
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        while(!Thread.currentThread().isInterrupted()){
+            try {
+                Utils.timer.schedule(Utils.taskQueue.take(),settings.getShowTrackDelay() * 1000L );
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
+        log.debug("Ui update schedule thread shut down.");
     }
 }
